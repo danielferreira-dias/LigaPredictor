5979a71d95335e65af465511b192c979f3563ba3
Daniel  Dias <daniel.dias@ludustechnology.com>
2025-02-03 22:46:19 +0000

    Clean frontend Vue template

---
+++ ligaPredictor-frontend-app/src/App.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+  <div class="w-full h-screen text-red-900 bg-zinc-950 flex justify-center items-center text-4xl">Hello World</div>
+</template>
+
+<style>
+
+</style>
