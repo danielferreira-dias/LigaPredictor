3a59e71afdc2ca6bb63c22d7bfde07c0867a12a5
Daniel  Dias <daniel.dias@ludustechnology.com>
2025-02-08 01:12:34 +0000

    changed logic of scraper

--- ligaPredictor-backend/scraper/models.py
+++ ligaPredictor-backend/scraper/models.py
@@ -1,73 +1,21 @@
 class GameInfo:
     def __init__(self, match_id):
         self.match_id = match_id
-        self.schedule = ""
         self.home_team = ""
         self.away_team = ""
+        self.home_team_id = ""
+        self.away_team_id = ""
         self.home_goals = 0
         self.away_goals = 0
-        self.xg_home = 0.0
-        self.xg_away = 0.0
-        self.possession_home = "0%"
-        self.possession_away = "0%"
-        self.goal_attempts_home = 0
-        self.goal_attempts_away = 0
-        self.shots_on_goal_home = 0
-        self.shots_on_goal_away = 0
-        self.shots_off_goal_home = 0
-        self.shots_off_goal_away = 0
-        self.shots_blocked_home = 0
-        self.shots_blocked_away = 0
-        self.great_opportunities_home = 0
-        self.great_opportunities_away = 0
-        self.corners_home = 0
-        self.corners_away = 0
-        self.shots_inside_the_area_home = 0
-        self.shots_inside_the_area_away = 0
-        self.shots_outside_the_area_home = 0
-        self.shots_outside_the_area_away = 0
-        self.shots_on_the_post_home = 0
-        self.shots_on_the_post_away = 0
-        self.goalkeeper_defenses_home = 0
-        self.goalkeeper_defenses_away = 0
-        self.freekicks_home = 0
-        self.freekicks_away = 0
-        self.offsides_home = 0
-        self.offsides_away = 0
-        self.fouls_home = 0
-        self.fouls_away = 0
-        self.yellow_cards_home = 0
-        self.yellow_cards_away = 0
-        self.red_cards_home = 0
-        self.red_cards_away = 0
-        self.throws_home = 0
-        self.throws_away = 0
-        self.passes_home = 0.0
-        self.passes_away = 0.0
-        self.passes_in_the_last_third_home = 0.0
-        self.passes_in_the_last_third_away = 0.0
-        self.crosses_home = 0.0
-        self.crosses_away = 0.0
-        self.effective_tackles_home = 0.0
-        self.effective_tackles_away = 0.0
-        self.tackles_home = 0
-        self.tackles_away = 0
-        self.interceptions_home = 0
-        self.interceptions_away = 0
-        self.win = ""
-        self.result = ""
+        self.winnerCode = 0
     
-
-    def set_result(self):
-        if self.home_goals > self.away_goals:
-            self.win = self.home_team
-            self.result = "Home"
-        elif self.home_goals < self.away_goals:
-            self.win = self.away_team
-            self.result = "Away"
-        else:
-            self.win = "Draw"
-            self.result = "Draw"
-
+    def set_statistic(self, key, home_value, away_value):
+        home_attr = f"{key}_home"
+        away_attr = f"{key}_away"
+        
+        # Set the statistic as attributes on the class
+        setattr(self, home_attr, home_value)
+        setattr(self, away_attr, away_value)
+        
     def to_dict(self):
         return self.__dict__
--- ligaPredictor-backend/scraper/scraper.py
+++ ligaPredictor-backend/scraper/scraper.py
@@ -1,200 +1,87 @@
-from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
-from models import GameInfo
-import requests
 import json
 import os
+import requests
+from models import GameInfo
+import http.client
 
-# Global Variables
-MapStatistics = {
-    "432": {"name": "xG", "type": float},
-    "12": {"name": "Possession", "type": str},
-    "34": {"name": "Goal Attempts", "type": int},
-    "13": {"name": "Shots on Goal", "type": int},
-    "14": {"name": "Shots off Goal", "type": int},
-    "158": {"name": "Shots Blocked", "type": int},
-    "459": {"name": "Great Opportunities", "type": int},
-    "16": {"name": "Corners", "type": int},
-    "461": {"name": "Shots inside the area", "type": int},
-    "463": {"name": "Shots outside the area", "type": int},
-    "457": {"name": "Shots on the Post", "type": int},
-    "19": {"name": "Goalkeeper Defenses", "type": int},
-    "15": {"name": "Freekicks", "type": int},
-    "17": {"name": "Offsides", "type": int},
-    "21": {"name": "Fouls", "type": int},
-    "23": {"name": "Yellow Cards", "type": int},
-    "22": {"name": "Red Cards", "type": int},
-    "18": {"name": "Throws", "type": int},
-    "342": {"name": "Passes", "type": float},
-    "467": {"name": "Passes in the last third", "type": float},
-    "433": {"name": "Crosses", "type": float},
-    "475": {"name": "Effective Tackles", "type": float},
-    "479": {"name": "Tackles", "type": int},
-    "434": {"name": "Interceptions", "type": int}
-}
-
-headers = {
-    "x-fsign": "SW9D1eZo"
-}
-
-def fetchGameURL():
-    urls_2024_2025 = [
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_184_0_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_184_1_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_184_2_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_184_3_0_pt_1",
-    ]
-    urls_2023_2024 = [
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_183_0_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_183_1_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_183_2_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_183_3_0_pt_1",
-    ]
-    urls_2022_2023 = [
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_176_0_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_176_1_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_176_2_0_pt_1",
-        "https://global.flashscore.ninja/20/x/feed/tr_1_155_UmMRoGzp_176_3_0_pt_1",
-    ]
-
-    season_url_map = {
-        "urls_2024_2025": urls_2024_2025,
-        "urls_2023_2024": urls_2023_2024,
-        "urls_2022_2023": urls_2022_2023,
-    }
-
-    all_merged_results = []
-
-    for season in season_url_map:
-        with ThreadPoolExecutor(max_workers=4) as executor:
-            results = list(executor.map(getGames, season_url_map[season]))
-
-        merged_results = [item for sublist in results for item in sublist]
-        createData(merged_results, season)
-        print(f"{len(merged_results)} from {season}")
-        all_merged_results.append((merged_results))
+def fetchSeasonCurrentRound(season_id):
+    url = f"https://www.sofascore.com/api/v1/unique-tournament/238/season/{season_id}/rounds"
     
-    all_merged_results_final = [item for sublist in all_merged_results for item in sublist]
-    print(len(all_merged_results_final))
-    createData(all_merged_results_final, "all_games")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
 
-def getGames(url):
-    gamesJSON = []  
-    response = requests.get(url, headers=headers)
+        fetched_Data = response.json()
+        lastround = fetched_Data['rounds'][-1]['round']
+    
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except KeyError as key_err:
+        print(f"Missing expected key in response: {key_err}")
+    except Exception as e:
+        print(f"Unexpected Error: ", e)
 
-    if response.status_code == 200:
-        matches = response.text.split("~AA÷")[1:]  
-        for match_section in matches:
-            parts = match_section.split("¬")
-            match_id = match_section.split("¬")[0]
+    return lastround
 
-             # Create a new GameInfo instance for this match
-            Game = GameInfo(match_id)
-            
-            for part in parts:
-                if "÷" in part:
-                    key, value = part.split("÷", 1)
-                    if key == "AD":  
-                        Game.schedule = datetime.fromtimestamp(int(value)).isoformat()
-                    elif key == "CX":
-                        Game.home_team = value
-                    elif key == "AF":
-                        Game.away_team = value
-                    elif key == "AG":
-                        Game.home_goals = int(value)
-                    elif key == "AH":
-                        Game.away_goals = int(value)
-            
-            # Determine the winner
-            Game.set_result()
+def fetchSeasonRoundsPlayed(season_id):
+    gameJSON = []
+    conn = http.client.HTTPSConnection("www.sofascore.com")
 
-            ## Request for Details
-            url_game_details = f"https://global.flashscore.ninja/20/x/feed/df_st_1_{match_id}"
-            response = requests.get(url_game_details, headers=headers)
+    for round_num  in range(1, fetchSeasonCurrentRound(season_id)):
+        url = f"https://www.sofascore.com/api/v1/unique-tournament/238/season/{season_id}/events/round/{round_num}"
 
-            # Verificar o status da resposta
-            if response.status_code == 200:
-                
-                data = response.text
-                # Substituir delimitadores
-                data = data.replace("¬", ",").replace("÷", ":").replace("~", "\n")
-                # Dividir o texto em seções
-                sections = data.split("\n")
+        try:
+            conn.request("GET", url)
 
-                result = []
-                for section in sections:
-                    if section:
-                        items = section.split(",")
-                        obj = {}
-                        for item in items:
-                            key_value = item.split(":")
-                            if len(key_value) == 2:
-                                key, value = key_value
-                                obj[key] = value
-                        
-                        if obj.get("SE") == "Jogo":
-                            collect = True
-                        elif obj.get("SE") == "1ª Parte":  
-                            break
+            response = conn.getresponse()
+            if response.status == 200:
+                fetched_Data = json.loads(response.read().decode("utf-8"))
+                for event  in fetched_Data['events']:
+                    if event.get("status", {}).get("type") == "finished":
+                        try:
+                            game = GameInfo(event["id"])
+                            game.home_team = event["homeTeam"]["name"]
+                            game.home_goals = event["homeScore"]["display"]
+                            game.home_team_id = event["homeTeam"]["id"]
+                            game.away_team = event["awayTeam"]["name"]
+                            game.away_goals = event["awayScore"]["display"]
+                            game.away_team_id = event["awayTeam"]["id"]
+                            game.winnerCode = event["winnerCode"]
 
-                        sd_value = obj.get("SD")
-
-                        if sd_value in MapStatistics:
-                            stat_info = MapStatistics[sd_value]
-                            stat_name = stat_info["name"]
-                            stat_type = stat_info["type"]
-
-
-                            try:
-                                sh_value = obj.get("SH", 0)
-                                si_value = obj.get("SI", 0)
-
-                                if "%" in str(sh_value):
-                                    home_value_text = str(sh_value).split("%")
-                                    home_value = float(home_value_text[0]) 
-                                else:
-                                    home_value = stat_type(sh_value)
-
-                                if "%" in str(si_value):
-                                    away_value_text = str(si_value).split("%")
-                                    away_value = float(away_value_text[0]) 
-                                else:
-                                    away_value = stat_type(si_value)
-
-                            except (ValueError, TypeError) as e:
-                                print(f"Error processing values for {stat_name}: {e}")
-                                home_value = None
-                                away_value = None
-
-                            # Update the corresponding attributes in the Game object
-                            attr_home = f"{stat_name.lower().replace(' ', '_')}_home"
-                            attr_away = f"{stat_name.lower().replace(' ', '_')}_away"
-                            
-                            if hasattr(Game, attr_home) and hasattr(Game, attr_away):
-                                setattr(Game, attr_home, home_value)
-                                setattr(Game, attr_away, away_value)
-
-                        if collect:
-                            result.append(obj)
+                            gameJSON.append(game.to_dict())
+                        except KeyError as key_err:
+                            print(f"Missing key in event data: {key_err}")
+                            continue  # Skip this event
             else:
-                print(f"Erro ao obter os dados. Status code: {response.status_code}")
+                print(f"Failed to fetch data, status code: {response.status}")
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except KeyError as key_err:
+            print(f"Missing expected key in response: {key_err}")
+        except Exception as e:
+            print(f"Unexpected Error: ", e)
 
-            # Add the updated GameInfoTemplate to the games list
-            gamesJSON.append(Game.to_dict())
-    else:
-        print(f"Erro ao obter os dados. Status code: {response.status_code}")
-    return gamesJSON
+    return gameJSON
 
-def createData(GameList, season):
-    data_dir = "LigaPredictor/ligaPredictor-data/liga_portugal"
+def upload_info_to_json(season):
+
+    data_dir = "./ligaPredictor-data/liga_portugal"
+    # Create the directory if it doesn't exist
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
-
-    # file_path = os.path.join(data_dir, f"game_data_{season}.json")
+    
+    # Define the file path for the season's JSON data
     file_path = os.path.join(data_dir, f"game_data_{season}.json")
-    with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(GameList, f, ensure_ascii=False, indent=4)
-    print(f"Dados armazenados - game_data_{season}.json")
+    
+    try:
+        # Fetch season data and write it to the JSON file
+        with open(file_path, "w", encoding="utf-8") as f:
+            season_data = fetchSeasonRoundsPlayed(season)  # Replace with your actual function call
+            json.dump(season_data, f, ensure_ascii=False, indent=4)
+        
+        print(f"Data for season {season} has been saved successfully at {file_path}.")
+    
+    except Exception as e:
+        print(f"An error occurred while uploading data: {e}")
 
-
-fetchGameURL()
+upload_info_to_json(63670)
